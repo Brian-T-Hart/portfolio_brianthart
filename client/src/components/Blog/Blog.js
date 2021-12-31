261306da1c8fd5565bf2b3233be62b2905516384
Brian-T-Hart <hart.briant@gmail.com>
2021-12-30 18:14:49 -0800

    added blog images

--- client/src/components/Blog/Blog.js
+++ client/src/components/Blog/Blog.js
@@ -8,14 +8,14 @@ const Blog = () => (
 		<div className="row flex-column flex-sm-row justify-content-around align-items-center mh-60">
 			<Link to="/blog/1">
 				<div className="d-flex flex-column mb-3">
-					<img alt="Web design graphic with computer screen, mouse and keyboard" className="blog-section-image" src="https://cdn.pixabay.com/photo/2018/05/18/15/30/webdesign-3411373_960_720.jpg" ></img>
+					<img alt="Web design graphic with computer screen, mouse and keyboard" className="blog-section-image" src="/images/web-dev.png" ></img>
 					<h4 className="m-2 text-center">Web Design Basics</h4>
 				</div>
 			</Link>
 
 			<Link to="/blog/2">
 				<div className="d-flex flex-column mb-3">
-					<img alt="Cloud computing graphic" className="blog-section-image" src="https://www.publicdomainpictures.net/pictures/290000/velka/web-hosting.jpg"></img>
+					<img alt="Cloud computing graphic" className="blog-section-image" src="/images/web-hosting.jpg"></img>
 					<h4 className="m-2 text-center">Cloud Computing</h4>
 				</div>
 			</Link>
